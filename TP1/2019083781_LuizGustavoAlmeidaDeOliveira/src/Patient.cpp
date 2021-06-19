7eb610694d0b45a7e846a31c724c5395c8420070
Luiz Almeida <luizorst@hotmail.com>
2021-06-19 20:35:15 -0300

    [ADD] initial implementation Clinic-Patient

---
+++ TP1/2019083781_LuizGustavoAlmeidaDeOliveira/src/Patient.cpp
@@ -0,0 +1,45 @@
+#include <Clinic.hpp>
+#include <Patient.hpp>
+#include <algorithm>
+#include <cmath>
+
+Patient::Patient(int _id, int _age, int posX, int posY, std::unordered_map<int, Clinic> &allClinics) {
+    this->id = _id;
+    this->age = _age;
+    this->positionXY = {posX, posY};
+    this->preferenceListClinicsById = sortClinicsByDistance(allClinics);
+    this->proposedToClinic = std::vector<bool>(allClinics.size(), false);
+}
+
+int Patient::getId() const { return this->id; };
+int Patient::getAge() const { return this->age; };
+bool Patient::isMatched() const { return this->matched; }
+bool Patient::proposedToAllClinics() const { return this->proposedToAll; }
+std::pair<int, int> Patient::getPosition() const { return this->positionXY; };
+std::vector<int> Patient::rankById() const { return this->preferenceListClinicsById; };
+
+std::vector<int> Patient::sortClinicsByDistance(std::unordered_map<int, Clinic> &clinics) {
+    std::vector<std::pair<int, float>> sortedByDistVec;
+
+    // criando vetor apenas com as IDs e distancia para fazer a ordenacao
+    for (auto &clinic : clinics) {
+        float distance = clinic.second.getPosition().first;
+        sortedByDistVec.push_back({clinic.first, distance});
+    }
+
+    std::sort(sortedByDistVec.begin(), sortedByDistVec.end(), [](std::pair<int, float> i, std::pair<int, float> j) {
+        if (i.second < j.second)
+            return true;
+        else if (i.second == j.second && i.first < j.first) // dist de i e j eh igual porem ID de i eh menor
+            return true;
+        else
+            return false; // a distancia de j eh menor ou a distancia e igual e o id de j eh menor
+    });
+
+    std::vector<int> rankVec(sortedByDistVec.size() + 1);
+    for (int i = 1; i < sortedByDistVec.size() + 1; ++i) {
+        rankVec[sortedByDistVec[i - 1].first] = i;
+    }
+
+    return rankVec;
+}
