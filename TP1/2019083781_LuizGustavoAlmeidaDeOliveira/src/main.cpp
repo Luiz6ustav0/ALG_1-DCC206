cc84f2e05fdbb1110661d357330b77eaf0642994
Luiz Almeida <luizorst@hotmail.com>
2021-06-19 16:56:32 -0300

    [FIX] rename folder

---
+++ TP1/2019083781_LuizGustavoAlmeidaDeOliveira/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+typedef long long ll;
+
+int main(){
+    
+    int numberOfClinics = 0, numberOfPatients = 0;
+
+    std::cin >> numberOfClinics;
+    int id = 0, capacity, x, y;
+    for (int i=0; i<numberOfClinics; ++i) {
+        std::cin >> capacity >> x >> y;
+        std::cout << id << " " << capacity << " " << x << " " << y << std::endl;
+        id++;
+    }
+
+    std::cin >> numberOfPatients;
+    int age = 0;
+    id = 0, x = 0, y = 0;
+    for (int i=0; i<numberOfPatients; ++i) {
+        std::cin >> age >> x >> y;
+        std::cout << id << " " << x << " " << y << std::endl;
+        id++;
+    }
+}
+
--- TP1/Luiz_Almeida_2019083781/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
-
-typedef long long ll;
-
-int main(){
-
-}
-
