2e18a179d1fe826af9f49cf5b495773da7a8a7f0
Luiz Almeida <luizorst@hotmail.com>
2021-06-23 00:23:04 -0300

    [ADD] more functionality tests

--- TP1/2019083781_LuizGustavoAlmeidaDeOliveira/tests/test.cpp
+++ TP1/2019083781_LuizGustavoAlmeidaDeOliveira/tests/test.cpp
@@ -1,16 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "Patient.hpp"
 #include "doctest.h"
+#include <Clinic.hpp>
+#include <Patient.hpp>
+#include <UtilsService.hpp>
 
-std::unordered_map<int, Clinic> clinicsData = std::unordered_map<int, Clinic>({
-            {1, Clinic(1, 4, 5, 2)}, {2, Clinic(2, 4, 0, 1)}, {3, Clinic(3, 4, 0, 5)}, {4, Clinic(4, 4, 5, 0)} 
-        }
-);
+std::unordered_map<int, Clinic<Patient>> clinicsData = std::unordered_map<int, Clinic<Patient>>({{1, Clinic<Patient>(1, 4, 5, 2)}, {2, Clinic<Patient>(2, 4, 0, 1)}, {3, Clinic<Patient>(3, 4, 0, 5)}, {4, Clinic<Patient>(4, 4, 5, 0)}});
 
-TEST_CASE("Testing basic instantiation") {
+/* ********************************
+    TESTS REGARDING THE PATIENT CLASS
+*/
+
+TEST_CASE("Basic instantiation contract for Patient") {
     // given
     int id = 0, age = 12, posX = 5, posY = 3;
-    
+
     // when
     Patient p(id, age, posX, posY, clinicsData);
 
@@ -21,10 +24,14 @@ TEST_CASE("Testing basic instantiation") {
     CHECK_EQ(posY, p.getPosition().second);
 }
 
+/* *****************************
+    TESTS REGARDING THE CLINIC CLASS
+*/
+
 TEST_CASE("Should order clinics by distance") {
     // given
     int id = 0, age = 12, posX = 0, posY = 0;
-    
+
     // when
     Patient p(id, age, posX, posY, clinicsData);
 
@@ -34,4 +41,80 @@ TEST_CASE("Should order clinics by distance") {
     CHECK_EQ(p.getRankById(3), 2);
     CHECK_EQ(p.getRankById(4), 3);
     CHECK_EQ(p.getRankById(1), 4);
+}
+
+TEST_CASE("Basic instantiation contract for Clinic") {
+    // given when
+    int id = 1, capacity = 4, posX = 3, posY = 4;
+    Clinic<Patient> c(id, capacity, posX, posY);
+
+    // then
+    CHECK_EQ(c.getId(), id);
+    CHECK_EQ(c.getCapacity(), capacity);
+    CHECK_EQ(c.getPosition().first, posX);
+    CHECK_EQ(c.getPosition().second, posY);
+}
+
+TEST_CASE("Should insert patient") {
+    // given
+    int id = 1, capacity = 4, posX = 3, posY = 4;
+    int idPatient = 13, age = 19, posXPatient = 13, posYPatient = 15;
+    Clinic<Patient> c(id, capacity, posX, posY);
+
+    // when
+    c.insertPatient({idPatient, age, posXPatient, posYPatient, clinicsData});
+
+    // then
+    CHECK_EQ(c.getRegisteredPatients()[0].getId(), idPatient);
+}
+
+TEST_CASE("Should order inserted patients by priority to allow constant lookup") {
+    // given
+    int id = 1, capacity = 4, posX = 3, posY = 4;
+    int idPatient = 13, age = 19, posXPatient = 13, posYPatient = 15;
+    int idPatient2 = 11;
+    int idPatient3 = 20, age3 = 70;
+    Clinic<Patient> c(id, capacity, posX, posY);
+
+    // when inserting should always sort by priority asc
+    c.insertPatient({idPatient, age, posXPatient, posYPatient, clinicsData});
+    c.insertPatient({idPatient2, age, posXPatient, posYPatient, clinicsData});
+    c.insertPatient({idPatient3, age3, posXPatient, posYPatient, clinicsData});
+
+    // then
+    CHECK_EQ(c.getRegisteredPatients()[0].getId(), idPatient); // lowest priority
+    CHECK_EQ(c.getRegisteredPatients()[1].getId(), idPatient2); // medium priority becaue of lower id
+    CHECK_EQ(c.getRegisteredPatients()[2].getId(), idPatient3); // highest priority because of age
+}
+
+/* *****************************
+TESTS REGARDING THE UTIL CLASS
+*/
+
+TEST_CASE("Should tell which patient has higher priority by age") {
+    // given
+    int idPatient = 13, age = 19, posXPatient = 13, posYPatient = 15;
+    int idPatient2 = 23, age2 = 39;
+    UtilService utilsService;
+
+    // when
+    Patient p(idPatient, age, posXPatient, posYPatient, clinicsData);
+    Patient v(idPatient2, age2, posXPatient, posYPatient, clinicsData);
+
+    // then
+    CHECK_EQ(utilsService.doesTheFirstHaveHigherPriority(p, v), false);
+}
+
+TEST_CASE("Should tell which patient has higher priority by id if age is equal") {
+    // given
+    int idPatient = 13, age = 39, posXPatient = 13, posYPatient = 15;
+    int idPatient2 = 23;
+    UtilService utilsService;
+
+    // when
+    Patient p(idPatient, age, posXPatient, posYPatient, clinicsData);
+    Patient v(idPatient2, age, posXPatient, posYPatient, clinicsData);
+
+    // then
+    CHECK_EQ(utilsService.doesTheFirstHaveHigherPriority(p, v), true);
 }
